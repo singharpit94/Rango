cedd0609952627c9d2768f7b1d4d0fc5c6ac58ba
Arpit Singh <arpitsinghnitd@gmail.com>
2015-10-30 00:41:56 +0530

    Did some changes

--- rango1/admin.py
+++ rango1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from rango.models import Category, Page
+
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Page)
--- rango1/urls.py
+++ rango1/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
-from rango1 import views
+from rango import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+	url(r'^about/',views.about,name='about'),
+	url(r'^add_category/$', views.add_category, name='add_category'), 
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+)
+	
